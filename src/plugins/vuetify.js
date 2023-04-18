b18e1ba0c94e3d19ed69ddcdfd95eb3465afa587
NoeJuza <noe.juzan.pro@gmail.com>
2023-04-18 19:09:50 +0200

    [ADD] First commit - vuetify.js has theme defined inside it

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,36 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+//#F6F3EC #E5E9Cd #DCE2B8 #4C5039
+
+const septemberTheme = {
+  dark: false,
+  colors: {
+    background: '#F6F3EC',
+    surface: '#E5E9CD',
+    primary: '#DCE2B8',
+    'primary-darken-1': '#DCE2B8',
+    secondary: '#4C5039',
+    'secondary-darken-1': '#4C5039',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  }
+}
+
+export default createVuetify(
+  {
+    theme: {
+      defaultTheme: 'septemberTheme',
+      themes: {
+        septemberTheme,
+      }
+    }
+  }
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+)
