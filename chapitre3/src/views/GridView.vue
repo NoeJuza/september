247f1138056356230c50a47eb1dad469435c9609
2074 <noe.juzan.pro@gmail.com>
2023-05-22 18:49:29 +0200

    made exercice responsiveGrids

---
+++ chapitre3/src/views/GridView.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-row no-gutters>
+        <v-col
+        align="center"
+        justify="center"
+        class="bg-indigo-lighten-4"
+        style="border: 1px solid indigo"
+        v-for="n in 8"
+        :key="n"
+        cols="12"
+        md="6"
+        lg="3"
+        >
+            <span>{{n}}</span>
+        </v-col>
+    </v-row>
+</template>
