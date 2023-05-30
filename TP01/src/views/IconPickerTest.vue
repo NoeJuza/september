92096812ba49b020d037d558895a839eaf5fc415
2074 <noe.juzan.pro@gmail.com>
2023-05-30 23:58:04 +0200

    ✨ [ADD] new component IconPicker and its test

---
+++ TP01/src/views/IconPickerTest.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+        <p>{{valueIcon}}</p>
+        <div class="arbitrary-size">
+            <IconPicker :currentlySelected="valueIcon" :updateCurrentlySelected="updateIcon"/>
+        </div>
+    </v-container>
+</template>
+<script setup>
+import { defineComponent, ref } from "vue";
+import IconPicker from "../components/IconPicker.vue";
+
+const valueIcon = ref("");
+const updateIcon = (value) => {
+    valueIcon.value = value;
+};
+
+console.log("test");
+defineComponent({
+    name:"IconPickerTest",
+    components:{
+        IconPicker
+    }
+})
+</script>
+<style>
+.arbitrary-size{
+    max-height: 300px;
+}
+</style>
