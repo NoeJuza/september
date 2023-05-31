5782ba88d83756d6af238c2fce8ead013bdd00e2
Adrien Sauthier <adrien.sauthier@edu.hefr.ch>
2023-05-31 10:57:25 +0200

    Modification des row/cols

--- TP01/src/components/TaskList.vue
+++ TP01/src/components/TaskList.vue
@@ -1,30 +1,34 @@
-
-
 <template>
     <v-app>
-        <v-row>
-            <v-col cols="4" class="d-flex justify-center align-center" style="background-color: #e5e9cd;">
-                <h1>TO DO</h1>
+        <v-row no-gutters>
+            <v-col cols="4" class="mb-0">
+                <v-container class="d-flex justify-center align-center bg-surface pa-2 rounded-lg">
+                    <h1>TO DO</h1>
+                </v-container>
             </v-col>
-            <v-col cols="4" class="d-flex justify-center align-center" style="background-color: #e5e9cd;">
-                <h1>DOING</h1>
+            <v-col cols="4">
+                <v-container class="d-flex justify-center align-center bg-surface pa-2 rounded-lg">
+                    <h1>DOING</h1>
+                </v-container>
             </v-col>
-            <v-col cols="4" class="d-flex justify-center align-center" style="background-color: #e5e9cd;">
-                <h1>DONE</h1>
+            <v-col cols="4">
+                <v-container class="d-flex justify-center align-center bg-surface pa-2 rounded-lg">
+                    <h1>DONE</h1>
+                </v-container>
             </v-col>
-        </v-row>
-        <v-row> 
-            <v-col cols="4" class="d-flex flex-column justify-center align-center" v-for="(task, index) in tasks" :key="index">
+
+            <v-col cols="4" class="d-flex flex-column justify-center align-center" v-for="(task, index) in tasks"
+                :key="index">
                 <Task :importance="task.importance" :type="task.type" :titre="task.titre" :date="task.date"></Task>
                 <v-btn @click="removeTask(index)">Supprimer</v-btn>
             </v-col>
         </v-row>
         <v-row>
             <v-col>
-                <input v-model="newTask.titre" placeholder="Titre"/>
-                <input v-model="newTask.importance" placeholder="Importance"/>
-                <input v-model="newTask.type" placeholder="Type"/>
-                <input v-model="newTask.date" placeholder="Date"/>
+                <input v-model="newTask.titre" placeholder="Titre" />
+                <input v-model="newTask.importance" placeholder="Importance" />
+                <input v-model="newTask.type" placeholder="Type" />
+                <input v-model="newTask.date" placeholder="Date" />
 
                 <v-btn @click="addTask">Ajouter</v-btn>
             </v-col>
@@ -38,15 +42,15 @@ import Task from './TAsk.vue';
 
 let tasks = ref([]);
 
-let newTask = ref({title: '', importance: '', type: '', date: ''});
+let newTask = ref({ title: '', importance: '', type: '', date: '' });
 
 function addTask() {
-  tasks.value.push({...newTask.value});
-  newTask.value = {title: '', importance: '', type: '', date: ''};
+    tasks.value.push({ ...newTask.value });
+    newTask.value = { title: '', importance: '', type: '', date: '' };
 }
 
 function removeTask(index) {
-  tasks.value.splice(index, 1);
+    tasks.value.splice(index, 1);
 }
 
 </script>
